5c4fd8d663b934a50beb3d927352ba9ae562aa7a
cvakodobro <vule0411@gmail.com>
2021-06-19 23:17:58 +0200

    component for most common

---
+++ web/components/MostCommon.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    disable-pagination
+    disable-sort
+    hide-default-footer
+    class="elevation-0"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      headers: [
+        { text: 'Part of the city', value: 'block' },
+        { text: 'Number', value: 'number' },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
