a64cb53415f4bf05323088740f8d3971013537c1
cvakodobro <vule0411@gmail.com>
2021-06-19 23:18:14 +0200

    adding most common on page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,5 +1,29 @@
 <template>
   <v-row justify="center" align="center">
+    <v-col cols="12" lg="4">
+      <v-card>
+        <v-card-title class="headline"> Most common in BGD</v-card-title>
+        <v-card-text>
+          <most-common :items="commonAll" />
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" lg="4">
+      <v-card>
+        <v-card-title class="headline"> Most common sell in BGD</v-card-title>
+        <v-card-text>
+          <most-common :items="commonSell" />
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" lg="4">
+      <v-card>
+        <v-card-title class="headline"> Most common rent in BGD</v-card-title>
+        <v-card-text>
+          <most-common :items="commonRent" />
+        </v-card-text>
+      </v-card>
+    </v-col>
     <v-col cols="12">
       <v-card>
         <v-card-title class="headline"> Realestate </v-card-title>
@@ -17,13 +41,24 @@
 </template>
 
 <script>
+import MostCommon from '../components/MostCommon.vue'
 export default {
+  components: { MostCommon },
   asyncData({ app }) {
+    // const data = app.$axios.get(`/most_common`)
+    // console.log(data)
+    // return {
+    //   commonSell: data.sell,
+    //   commonRent: data.rent,
+    //   commonAll: data.all,
+    // }
     return app.$axios
-      .$get('/realestate')
-      .then((realestate) => {
+      .$get('/most_common')
+      .then((data) => {
         return {
-          realestate,
+          commonSell: data.sell,
+          commonRent: data.rent,
+          commonAll: data.all,
         }
       })
       .catch((error) => console.log(error))
@@ -31,6 +66,9 @@ export default {
   data() {
     return {
       realestate: [],
+      commonSell: [],
+      commonRent: [],
+      commonAll: [],
       headers: [
         { text: 'Location', value: 'location' },
         { text: 'URL', value: 'url' },
